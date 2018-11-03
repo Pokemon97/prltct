03827efe7d4f4772abad4450ee77c93398b552bb
unknown <tuananh97aowvn@gmail.com>
2018-11-03 23:40:31 +0700

    first commit

---
+++ resources/views/header.blade.php
@@ -0,0 +1,9 @@
+<div id="header" class="color-div">
+		<div class="container">
+			<div class="row">
+				<div class="row-sm-8" style="text-align: center; background-color: #636b6f; color: #FFF; padding: 10px">
+					<span>Header</span>
+				</div>				
+			</div>
+		</div> <!-- .container -->
+	</div> <!-- #footer -->
---
+++ routes/web.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+    
+});
+//trang admin
+Route::get('index', [
+	'as'=>'trang-chu',
+	'uses'=>'PageController@getIndex'
+]);
+
+Route:: group(['prefix'=>'admin'], function() {
+	
+	Route::group(['prefix'=>'theloai'], function() {
+		Route::get('danhsach', 'TheLoaiController@getDanhSach');
+
+		Route::get('them', 'TheLoaiController@getThem');
+
+		Route::post('them', 'TheLoaiController@postThem');
+
+		Route::get('sua/{id}', 'TheLoaiController@getSua');
+
+		Route::post('sua/{id}', 'TheLoaiController@postSua');
+
+		Route::get('xoa/{id}', 'TheLoaiController@getXoa');
+
+	});
+
+	Route::group(['prefix'=>'sanpham'], function() {
+		Route::get('danhsach', 'SanPhamController@getDanhSach');
+
+		Route::get('them', 'SanPhamController@getThem');
+
+		Route::post('them', 'SanPhamController@postThem');
+
+		Route::get('sua/{id}', 'SanPhamController@getSua');
+
+		Route::post('sua/{id}', 'SanPhamController@postSua');
+
+		Route::get('xoa/{id}', 'SanPhamController@getXoa');
+
+	});
+
+	
+});
